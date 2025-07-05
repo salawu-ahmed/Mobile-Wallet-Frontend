c70bed2c94167ed7fbff3553eefc408804d41bab
ahmed <a.salawu@outlook.com>
2025-07-05 21:14:21 +0100

    import updates

--- styles/home.styles.js
+++ styles/home.styles.js
@@ -1,6 +1,6 @@
 // styles/home.styles.js
 import { StyleSheet } from "react-native";
-import COLORS  from "../../constants/colors.js";
+import { COLORS }  from "../constants/colors.js";
 
 export const styles = StyleSheet.create({
   container: {
