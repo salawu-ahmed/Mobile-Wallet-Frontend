6981284ebef7e1a4ba13edb81bf7eb5ddafe0a4b
ahmed <a.salawu@outlook.com>
2025-07-07 16:11:25 +0100

    updated the SignOutButton

--- components/SignOutButton.jsx
+++ components/SignOutButton.jsx
@@ -1,23 +1,37 @@
 import { useClerk } from "@clerk/clerk-expo"
+import { Ionicons } from "@expo/vector-icons"
 import * as Linking from 'expo-linking'
-import { Text, TouchableOpacity } from "react-native"
+import { Alert, Text, TouchableOpacity } from "react-native"
+import { styles } from "../styles/home.styles"
+import { COLORS } from "../constants/colors"
+import { useState } from "react"
 
 export const SignOutButton = () => {
     //  use useClerk() to access the signOut() function 
     const {signOut} = useClerk()
+    // const [loggingOut, setIsLoggingOut] = useState(false)
     const handleSignOut = async () => {
-        try {
-            await signOut()
-            // redirect user to your desired page after sign-out
-            Linking.openURL(Linking.createURL('/'))
-        } catch (error) {
-            
-        }
+        Alert.alert(
+            'Confirm Logout Action',
+            'Are you sure you want to log out?',
+            [
+                {
+                    text: 'Sign out',
+                    style: 'destructive',
+                    onPress: signOut
+                },
+                {
+                    text: 'Cancel',
+                    style: 'cancel'
+                }
+            ]
+        )
     }
 
+
     return (
-        <TouchableOpacity onPress={handleSignOut}>
-            <Text>Sign out</Text>
+        <TouchableOpacity style={styles.logoutButton} onPress={handleSignOut}>
+            <Ionicons name="log-out-outline" size={22} color={COLORS.text}/>
         </TouchableOpacity>
     )
 }
